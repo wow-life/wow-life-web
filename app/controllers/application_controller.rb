114b4a4cf7ceff5c6db906715dff54147cbf3f98
Vikhyath <vikhyatc@thoughtworks.com>
2016-07-25 00:27:27 +0530

    Added content for contact us page <Viky>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-    render pages_home_path
+    render :template => 'pages/home.html.erb'
+
+  end
+
+  def contact_us
+    render :template => 'pages/contact_us.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'application#home'
-  get 'pages/home'
+  get 'contact_us', to: 'application#contact_us', as: 'contact_us'
 end
